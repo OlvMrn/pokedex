cbc7ba98748131a4aae3e9b1378b56d8c9009f11
OlvMrn <olivier.morin91@gmail.com>
2023-10-10 11:18:11 +0200

    Quete React Basics 05, ajout des proptypes sur le composant pokemoncard

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 function PokemonCard(props) {
   console.log(props);
   return(
@@ -6,6 +8,13 @@ function PokemonCard(props) {
       <figcaption>{props.pokemon.name}</figcaption>
     </figure>
   )
+}
+
+PokemonCard.PropTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imgSrc: PropTypes.string.isRequired,
+  }).isRequired,
 }
 
 export default PokemonCard;
